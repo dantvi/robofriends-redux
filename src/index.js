5d50aea9652a6a4aca6c71551ba905930c06d047
dantvi <daniel.tving@gmail.com>
2024-12-31 21:15:35 +0100

    redux connect

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import ReactDOM from 'react-dom'; The new way to import createRoot:
 import { createRoot } from "react-dom/client";
-import { Provider, connect } from 'react-redux';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
