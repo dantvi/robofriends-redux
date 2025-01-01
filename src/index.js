bc960abc6f2d9f2e265ef337843cda11309e518e
dantvi <daniel.tving@gmail.com>
2025-01-01 17:56:55 +0100

    redux middleware

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 // import ReactDOM from 'react-dom'; The new way to import createRoot:
 import { createRoot } from "react-dom/client";
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import './index.css';
 import App from './containers/App';
 import { searchRobots } from './reducers';
 import 'tachyons';
 // import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
 
 const root = createRoot(document.getElementById('root'));
 root.render(
